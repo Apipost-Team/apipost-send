2ef2b479ac62ffa03b008a80c99f983653194230
宣博文 <1030698842@qq.com>
2022-07-10 20:07:10 +0800

    pull

--- src/index.ts
+++ src/index.ts
@@ -5,25 +5,3 @@ import ApipostRequest from './ApipostRequest'
 
 
 export default ApipostRequest
-
-
-
-base64Body: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAZQ
-code: 200
-cookies: { BAIDUID: '65F0C08054CC9A00BCB8C693570BE5CC:FG=1' }
-fitForShow: "Image"
-header: (12)[{… }, {… }, {… }, {… }, {… }, {… }, {… }, {… }, {… }, {… }, {… }, {… }]
-headers: { accept - ranges: 'bytes', cache - control: 'max-age=315360000', content - length: '12802', content - type: 'image/png', date: 'Thu, 07 Jul 2022 03:53:39 GMT', … }
-json: { }
-raw: { status: 'OK', responseTime: 51, type: 'png', responseText: '' }
-rawBody: "/root/response_e258a59c-117a-4ee0-9041-3cc3f052d753.png"
-rawCookies: []
-resCookies: [{… }]
-resHeaders: { accept - ranges: 'bytes', cache - control: 'max-age=315360000', content - length: '12802', content - type: 'image/png', date: 'Thu, 07 Jul 2022 03:53:39 GMT', … }
-resMime: { ext: 'png', mime: 'image/png' }
-responseSize: 12.5
-responseTime: 51
-status: "OK"
-stream: { type: 'Buffer', data: Array(12802) }
-target_id: "e258a59c-117a-4ee0-9041-3cc3f052d753"
-timingPhases:
